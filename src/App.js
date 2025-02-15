a854c7cc5d72a4d3a5a4782bd1b66bfea7cc2591
satya4147 <m.satya4147@gmail.com>
2025-02-16 01:06:59 +0530

    Post Comments

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Comments from './Component/CommentList';
+
+function App() {
+  return (
+    <div className="App">
+     <Comments />
+    </div>
+  );
+}
+
+export default App;
